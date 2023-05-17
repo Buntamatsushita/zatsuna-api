c896fecc8468d3e5f50b5eecd00a32e389a27f51
Buntako <buntamatsushita@gmail.com>
2023-05-17 13:26:54 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, UploadFile,File
+import uvicorn
+
+import shutil
+import os
+
+import os
+import numpy as np
+from PIL import Image
+import keras
+from keras.models import load_model
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World"}
+
+@app.get("/bad/")
+async def root():
+    return {"message":"bad"}
+
+@app.post("/files/")
+async def file(file: bytes = File(...)):
+    content = file.decode('utf-8')
+    formatfile = content.split('\n')
+    return {'filedetail': formatfile}
+
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+    if file:
+        filename = file.filename
+        fileobj = file.file
+        upload_dir = open(os.path.join("../uploads/", filename),'wb+')
+        shutil.copyfileobj(fileobj, upload_dir)
+        upload_dir.close()
+        return {"アップロードファイル名": filename}
+    return {"Error": "アップロードファイルが見つかりません。"}
+
+@app.post("/cat_or_dog/")
+async def upload_file(file: UploadFile = File(...)):
+    if file:
+        filename = file.filename
+        fileobj = file.file
+        
+        result = catORdog(fileobj)
+        
+        if result[0] == 0:
+            result[0] = "dog"
+        else:
+            result[0] = "cat"
+
+        return {"filename": filename, "result": result[0], "prd":result[1]}
+    return {"Error": "アップロードファイルが見つかりません。"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
+    
+def catORdog(fileobj):
+        imsize = (64, 64)
+
+        def load_image(path):
+            img = Image.open(path)
+            img = img.convert('RGB')
+            # 学習時に、(64, 64, 3)で学習したので、画像の縦・横は今回 変数imsizeの(64, 64)にリサイズします。
+            img = img.resize(imsize)
+            # 画像データをnumpy配列の形式に変更
+            img = np.asarray(img)
+            img = img / 255.0
+            return img
+
+        model = load_model("../src/cnn.h5")
+        img = load_image(fileobj)
+        prd = model.predict(np.array([img]))
+        prelabel = np.argmax(prd, axis=1)
+        result = []
+        result.append(prelabel)
+        result.append(str(prd[0]))
+        return result
